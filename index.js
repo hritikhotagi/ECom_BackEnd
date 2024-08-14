6f0cde5618e79cadf43ba1c19b938518c4ce6a75
Hritik S Hotagi <hritikhotagi@gmail.com>
2024-08-14 22:44:33 +0530

    added vercel file

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/', routes);
 
 // Start the server
-app.listen(3001, () => {
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
     console.log("Server running on port 3001");
 });
